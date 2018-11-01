ad0ad31dfac60c3d2697c0fb9bb5f1a560e4ef49
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-01 12:32:41 -0600

    basic app, blank components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
 import React from 'react'
-//import Form from './Form
-//import Card from './Card
+import Form from './Form'
+import Card from './Card'
+
+class App extends React.Component {
+  state ={cards:[], editing: null}
+
+  showCards = () => {
+    const {cards} = this.state
+    return (
+      <div className="row">
+        { cards.map( card =>
+            <Card 
+              key={card.id}
+              {...card}
+            />
+          )
+        }
+      </div>
+    )
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <Form  editing={editing}/>
+        { cards.length ? this.showCards() : <h1 className="center">No Cards</h1> }
+      </div>
+    )
+  }
+}
+
+export default App
