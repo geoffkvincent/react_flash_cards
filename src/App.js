3c75a116ec60ea5e1b7b3b77b5176184790441d9
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-01 12:11:28 -0600

    base app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,3 @@
-import React from 'react';
-import CardForm from './components/CardForm';
-import CardContent from './components/CardContent'
-
-
-class App extends React.Component {
-  state = { cards: [] }
-
-  addCard = (question) => {
-
-  }
-
-  updateCard = (id) => {
-
-  }
-
-  deleteCard = (id) => {
-
-  }
-
-  render() {
-    return (
-      <div>
-        <CardForm addCard={this.addCard} />
-        <CardContent
-          cards={this.state.cards}
-          updateCard={this.updateCard}
-          deleteCard={this.deleteCard}
-        />
-      </div>
-    );
-  }
-}
-
-export default App;
+import React from 'react'
+//import Form from './Form
+//import Card from './Card
